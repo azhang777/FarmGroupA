9f15255522232c28e40905fb15ea465ca5cd49b3
Kalil <Kalil@anas-mbp.myfiosgateway.com>
2023-09-28 15:12:09 -0400

    update Farm, ChickenCoop, FarmHouse, Stable, Field

--- src/main/java/groupa/farm/Stable.java
+++ src/main/java/groupa/farm/Stable.java
@@ -1,4 +1,22 @@
 package groupa.farm;
 
+import groupa.animal.Horse;
+
+import java.util.ArrayList;
+
 public class Stable {
+
+    private ArrayList<Horse> horses;
+
+    public Stable() {
+        this.horses = new ArrayList<>();
+    }
+
+    public ArrayList<Horse> getHorses() {
+        return horses;
+    }
+
+    public void addHorse(Horse horse) {
+        this.horses.add(horse);
+    }
 }
