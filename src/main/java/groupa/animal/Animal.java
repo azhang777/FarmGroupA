ef43740a107d3c6aa06f247c0b10cc1ca4e11be2
cibezim <git@github.com:cibezim/git_test.git>
2023-09-28 14:51:41 -0400

    Adds Animal Class

--- src/main/java/groupa/animal/Animal.java
+++ src/main/java/groupa/animal/Animal.java
@@ -1,5 +1,30 @@
 package groupa.animal;
 
-public abstract class Animal {
+import groupa.interfaces.Eater;
+
+public abstract class Animal implements Eater {
+    private String sound;
+    private String name;
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+
+    public Animal(String sound, String name) {
+        this.sound = sound;
+        this.name = name;
+    }
+
+    public abstract void makeNoise();
+
+
+    @Override
+    public void eat(Edible food) {
+    }
+}
 
 
