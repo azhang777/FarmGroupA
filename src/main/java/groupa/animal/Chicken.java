7d6fefb05f63aaeeed709070466e6f437cb74be8
cibezim <git@github.com:cibezim/git_test.git>
2023-09-28 21:54:14 -0400

    Adds Chicken class

--- src/main/java/groupa/animal/Chicken.java
+++ src/main/java/groupa/animal/Chicken.java
@@ -1,3 +1,35 @@
 package groupa.animal;
+import groupa.interfaces.Edible;
+import groupa.interfaces.Produce
+public class Chicken extends Animal implements Produce, Edible {
 
-public class Chicken
+    private boolean hasBeenFertilized;
+
+    public Chicken(String name, int age, boolean hasBeenFertilized) {
+        super(name, age);
+        this.hasBeenFertilized = hasBeenFertilized;
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Chicken clucks");
+    }
+
+    @Override
+    public void eat(Edible food) {
+        if (food instanceof EdibleEgg) {
+            System.out.println(getName() + " the Chicken is produces an EdibleEgg.");
+        } else {
+            System.out.println(getName() + " the Chicken doesn't produce any eggs.");
+        }
+    }
+
+    @Override
+    public Edible yieldProduce() {
+        if (!hasBeenFertilized) {
+            return new EdibleEgg();
+        } else {
+            return null;
+        }
+    }
+}
