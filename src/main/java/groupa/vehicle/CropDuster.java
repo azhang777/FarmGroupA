7aac0986973338f162a57c1d4977d19df135eead
W-H14 <wyatthall390@gmail.com>
2023-10-02 17:43:43 -0400

    adds fertilize

--- src/main/java/groupa/vehicle/CropDuster.java
+++ src/main/java/groupa/vehicle/CropDuster.java
@@ -2,15 +2,23 @@ package groupa.vehicle;
 
 import groupa.farm.Field;
 import groupa.interfaces.FarmVehicle;
+import groupa.farm.CropRow;
+
+import java.util.ArrayList;
 
 public class CropDuster extends Aircraft implements FarmVehicle {
     private void fertilize(Field field){
-
+    ArrayList<CropRow> cropRows = field.getCropRows();
+    for (CropRow cropRow : cropRows) {
+        cropRow.fertilizeAllCrops();
+    }
 
     }
     @Override
     public boolean operate() {
-        return false;
+        fly();
+
+        return true;
     }
 
     @Override
