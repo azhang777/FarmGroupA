61c996d6d29e2d6c38290f57cf9274298576e0bd
W-H14 <wyatthall390@gmail.com>
2023-10-02 17:37:41 -0400

    adds harvest

--- src/main/java/groupa/vehicle/Tractor.java
+++ src/main/java/groupa/vehicle/Tractor.java
@@ -1,11 +1,16 @@
 package groupa.vehicle;
-
+import groupa.farm.CropRow;
 import groupa.farm.Field;
 import groupa.interfaces.FarmVehicle;
+import java.util.ArrayList;
 
 public class Tractor extends Vehicle implements FarmVehicle {
     private void harvest(Field field){
+        ArrayList<CropRow> cropRows = field.getCropRows();
 
+        for (CropRow cropRow : cropRows) {
+            cropRow.harvestAllCrops();
+        }
 
     }
 
