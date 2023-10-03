6cafb7ad863a58abc7a8e86d19b7b61a58660388
Andy <andyzhanggm@gmail.com>
2023-10-02 23:50:40 -0400

    Changes to CropRow

--- src/main/java/groupa/Main.java
+++ src/main/java/groupa/Main.java
@@ -9,36 +9,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Basket basket = Basket.getInstance();
-        CornStalk cornStalk = new CornStalk();
-        TomatoPlant tomatoPlant = new TomatoPlant();
-        cornStalk.setHasBeenFertilized(true);
-        cornStalk.setHasBeenHarvested(true);
-        tomatoPlant.setHasBeenFertilized(true);
-        tomatoPlant.setHasBeenHarvested(true);
 
-        cornStalk.yield();
-        tomatoPlant.yield();
-        cornStalk.pick();
-        tomatoPlant.pick();
-        basket.toString();
-
-        Chicken chicken = new Chicken("Kenny",2,false);
-        //running into issues here
-        chicken.yield();
-        chicken.collectEgg();
-        chicken.eat();
-        System.out.println("test");
-        /*
-        works! Basket will retrieve corn and tomato from their arraylist.
-        now test takeFrom by calling it from an animal that eats()
-         */
-
-        /*
-        best solution?: instead of having individual array
-         */
-
-
-        CropRow cropRow = new CropRow(new ArrayList<Crop>());
     }
 }
